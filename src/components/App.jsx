34e9d2f91f4f58db397a7ee92092cfbe235e404a
Oleksandr <alex.vovchok@gmail.com>
2023-04-17 12:09:38 +0300

    step2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,14 @@ import { nanoid } from 'nanoid'
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      {id:nanoid() , name: 'Rosie Simpson', number: '459-12-56'},
+    {id: nanoid(), name: 'Hermione Kline'},
+    {id: nanoid(), name: 'Eden Clements'},
+    {id: nanoid(), name: 'Annie Copeland'},
+    ],
+    name: '',
+    number: '',
   };
 
   loginInputId = nanoid();
@@ -14,10 +20,23 @@ class App extends Component {
     console.log(event.target.value);
     this.setState({name:event.target.value})
   }
+  handleChangeNumber = event => {
+    console.log(event.target.value);
+    this.setState({number:event.target.value})
+  }
 
   handleSubmit = event => {
     event.preventDefault();
       console.log(this.state);
+      console.log(this.state.contacts);
+      this.setState(
+        prevState => ({
+          contacts: [
+            ...prevState.contacts,
+          { id: nanoid(), name: this.state.name, number: this.state.number},
+          ]
+        })
+      )
       this.reset();
   }
 
@@ -42,15 +61,32 @@ class App extends Component {
     required
   />
        </label>
+       Number
+       <label htmlFor="">
+
+       <input
+  type="tel"
+  name="number"
+  value={this.state.number}
+  onChange={this.handleChangeNumber}
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+/>
+       </label>
   <button type="submit">Add contact</button>
        </form>
        <h2>Contacts</h2>
-       {/* <ul>
-        {this.state.contacts.map(contact => (
-          <li>{contact.name}</li>
+       <ul>
+        {this.state.contacts.map(({id, name, number}) => (
+          <li
+          key={id}>
+            <p>{name}:</p>
+            <p>{number}</p>
+          </li>
         ))}
         
-       </ul> */}
+       </ul>
       </div>
     );
   }
